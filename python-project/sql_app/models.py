0e7706f581f3f1c25af2207a19985dbe7ee9fa0a
jaepass <jaeriahpasseri@gmail.com>
2023-06-02 16:22:11 -0700

    Add new class objects

--- python-project/sql_app/models.py
+++ python-project/sql_app/models.py
@@ -1,33 +1,39 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-
 class Location(Base):
     __tablename__ = "locations"
+
     id = Column(Integer, primary_key=True, index=True)
     city = Column(String)
     country = Column(String)
     description = Column(String)
     image_url = Column(String)
 
+    programs = relationship("Program")
 
-# from sqlalchemy import String, ForeignKey, Column, Integer
-# from .database import Base
+class Program(Base):
+    __tablename__ = "programs"
 
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    location_id = Column(Integer, ForeignKey("locations.id"))
+    vendor_id = Column(Integer, ForeignKey("vendors.id"))
+    category_id = Column(Integer, ForeignKey("category.id"))
 
-# class Program(Base):
-#     __tablename__ = "programs"
+class Vendor(Base):
+    __tablename__ = "vendors"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String, index=True)
-#     category_id = Column(String, index=True)
-#     country = Column(String, index=True)
-#     city = Column()
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
 
-# class Program(Base):
-#   __tablename__ = "programs"
+class Category(Base):
+    __tablename__ = "categories"
 
-#   id = Column(Integer, primary_key=True, index=True)
-#   name = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
